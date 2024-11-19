0a4491e17bd9fb78f16916c61848c7b4a715c85b
Koushambha Das <122219149+KOUSHAMBHA15IT@users.noreply.github.com>
2024-11-19 17:44:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+const no = document.getElementById("n");
+
+no.addEventListener("mouseover", () =>{
+    no.textContent="YES"
+});
+
+no.addEventListener("mouseout", () =>{
+    no.textContent="NO"
+});
